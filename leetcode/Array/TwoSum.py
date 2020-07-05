f058d4af8640c28529baf86f89e5072be6518354
k-wachira <k.wachira@alustudent.com>
2020-07-05 21:12:40 +0200

    Level - Easy

--- leetcode/Array/TwoSum.py
+++ leetcode/Array/TwoSum.py
@@ -1,3 +1,20 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+:: Approach
+
+Assume the first number is 'i' , this makes the second number target - i
+before searching for second number in nums list replace i with a placeholder
+    (number that cant be second  number eg 0.1) in order to maintain the indexing of the numbers
+if number is found append its index and that of i
+
+Else Replace the placeholder( fake number) with its original value with is i
+
+
+
+"""
 
 def twoSum(nums, target):
     returnList =[]
