b041262a88a726866a17aaec6fb0518a7a8fe930
k-wachira <k.wachira@alustudent.com>
2020-07-06 20:10:35 +0200

    Level -Easy'

---
+++ leetcode/Array/RotateArray.py
@@ -0,0 +1,27 @@
+"""
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Link :: https://leetcode.com/problems/rotate-array/
+
+steps ::
+loop though the list k times
+On each loop take the last element and insert it to index 0
+pop the last element which has been duplicated on index 0
+
+"""
+
+
+def rotate( nums ,k):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    for i in range(k):
+        lastElement = nums[len(nums) - 1]
+        print(lastElement)
+        nums.insert(0, lastElement)
+        nums.pop()
+        print(nums)
+
+nums =  [1,2,3,4,5,6,7]
+k = 3
+rotate(nums, k)
