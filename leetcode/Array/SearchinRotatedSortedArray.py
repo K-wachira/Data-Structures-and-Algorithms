ce92efbef4d94372e836cb61d9f4f59418d66422
k-wachira <k.wachira@alustudent.com>
2020-07-12 17:35:53 +0200

    Level- Medium

--- leetcode/Array/SearchinRotatedSortedArray.py
+++ leetcode/Array/SearchinRotatedSortedArray.py
@@ -3,45 +3,34 @@ import math
 
 
 def  search( nums, target):
-    if len(nums) == 0: return -1
-
-    pivot = min(nums)
-    if target == pivot:
-        print(nums.index(pivot))
-        return nums.index(pivot)
-    elif target == nums[0]:
-        return 0
-    elif target == nums[len(nums) - 1]:
-        return len(nums) - 1
-    elif target > pivot and target <= nums[len(nums) - 1]:
-        number = binarysearch(target, nums[nums.index(pivot):])
-        print(number)
-        print(nums.index(number))
-    elif target >= nums[0] and target <= nums[nums.index(pivot) - 1]:
-        number = binarysearch(target, nums[: nums.index(pivot)])
-        print(nums.index(number))
-
-
-    else:
+    if not nums:
         return -1
 
+    low, high = 0, len(nums) - 1
 
-def binarysearch( target, nums):
-    middlepoint = int(len(nums) / 2)
-    if target == nums[middlepoint]:
-        return (middlepoint)
-    elif target > nums[middlepoint]:
-        binarysearch(target, nums[middlepoint:])
-    elif target > nums[middlepoint]:
-        binarysearch(target, nums[:middlepoint])
+    while low <= high:
+        mid = (low + high) // 2
+        if target == nums[mid]:
+            print(mid)
+            return mid
 
+        if nums[low] <= nums[mid]:
+            if nums[low] <= target <= nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        else:
+            if nums[mid] <= target <= nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
 
-
-
-
-
+    return -1
 
 nums = [4,5,6,7,0,1,2]
-target = 7
+target = 2
 
 search(nums, target)
+
+
+print(3//2)
