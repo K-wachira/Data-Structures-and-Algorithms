c1df5478b4fc2e9685d3288a5fea5a405deee542
k-wachira <k.wachira@alustudent.com>
2020-07-13 16:37:13 +0200

    Level-Easy

---
+++ leetcode/Array/searchInsert.py
@@ -0,0 +1,36 @@
+"""
+https://leetcode.com/problems/search-insert-position/submissions/
+
+"""
+
+def searchInsert(nums, target):
+    if not nums: return 0
+    low, high = 0, len(nums)-1
+    if target < nums[low]: return 0
+    elif target > nums[high]: return high+1
+
+
+    while low <= high:
+        mid = (low + high) // 2
+        if target == nums[mid]:  return mid
+
+        if nums[mid]< target < nums[mid+1]: return  mid+1
+
+
+        if nums[low] <= nums[mid]:
+            if nums[low] <= target <= nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        else:
+            if nums[mid] <= target <= nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+
+
+
+nums = []
+target = 9
+searchInsert(nums, target)
