f3de0acd9dc2c6f7f78a0a099d33f38057435d2f
k-wachira <k.wachira@alustudent.com>
2020-07-22 11:19:34 +0300

    first try

--- Algorithmic-toolbox-Coursera/week2/LastDigitofaLargeFinonacciNumber.py
+++ Algorithmic-toolbox-Coursera/week2/LastDigitofaLargeFinonacciNumber.py
@@ -5,7 +5,8 @@ def FibanacciNumber(n):
     while i < n:
         ans.append(ans[len(ans)-1] + ans[len(ans)-2])
         i+=1
-    return ans[:n]
+    ans = ans[:n]
+    return ans[len(ans)-1]
 if __name__ == '__main__':
     input_n = int(input())
     print(FibanacciNumber(input_n))
