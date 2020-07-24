625cc3daaa103cddb09c28a8a9c0e35ba79ac065
k-wachira <k.wachira@alustudent.com>
2020-07-24 10:41:32 +0300

    Optimal Solution

---
+++ leetcode/Array/medium/SingleNumberIII2.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/single-number-iii/submissions/
+
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice.
+Find the two elements that appear only once.
+
+"""
+def singleNumber(nums):
+    nonDublicates = set()
+    for i in nums:
+        if i in nonDublicates:
+            nonDublicates.remove(i)
+        else:
+            nonDublicates.add(i)
+    return list(nonDublicates)
+nums = [1,2,1,3,2,5]
+print(singleNumber(nums))
