808489d92ff08ad67a1de1888ce5983337985fe8
k-wachira <k.wachira@alustudent.com>
2020-07-26 00:03:29 +0300

    Level_Hard

---
+++ leetcode/Array/hard/FirstMissingPositive.py
@@ -0,0 +1,24 @@
+def firstMissingPositive(nums):
+    if not nums: return 1
+    nums.sort()
+
+    counta = 1
+    for  n in (set(nums)):
+        if n > 0:
+            if n != counta:
+                return counta
+            counta += 1
+        if sum(nums) < 1: return 1
+    return max(nums) + 1
+            
+
+
+
+
+
+if __name__ == '__main__':
+    listelements = [int(k) for k in input().split()]
+    (firstMissingPositive(listelements))
+
+
+
