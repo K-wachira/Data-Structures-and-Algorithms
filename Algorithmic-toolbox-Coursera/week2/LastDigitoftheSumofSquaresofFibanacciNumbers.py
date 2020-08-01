391556b9b0c7398813b70e53aa6885eeb3a61f93
k-wachira <k.wachira@alustudent.com>
2020-08-01 21:51:37 +0200

    With better Explanation

--- Algorithmic-toolbox-Coursera/week2/LastDigitoftheSumofSquaresofFibanacciNumbers.py
+++ Algorithmic-toolbox-Coursera/week2/LastDigitoftheSumofSquaresofFibanacciNumbers.py
@@ -1,20 +1,39 @@
 from sys import stdin
 
+def fibonacci_sum_squares_naive(n):
+    if n <= 1:
+        return n
 
-def FibanacciNumber(n):
-    if n == 0: return 0
-    ans = [0,1]
-    i =1
-    listofSquares =[]
-    while i < n:
-        ans.append(ans[len(ans)-1] + ans[len(ans)-2])
-        i+=1
+    previous , current, sums= 0 ,1,1
 
-    for i in ans:
-        listofSquares.append(i*i)
+ 
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+        sums += current * current
 
-    digit = str(sum(listofSquares))
-    return (int(digit[-1]))
+    return sums % 10
+
 if __name__ == '__main__':
-    input_n =int(input())
-    print(FibanacciNumber(input_n))
+    n = int(stdin.read())
+    print(fibonacci_sum_squares_naive(n))
+    
+    
+    
+# from sys import stdin
+# def FibanacciNumber(n):
+#     if n == 0: return 0
+#     ans = [0,1]
+#     i =1
+#     listofSquares =[]
+#     while i < n:
+#         ans.append(ans[len(ans)-1] + ans[len(ans)-2])
+#         i+=1
+
+#     for i in ans:
+#         listofSquares.append(i*i)
+
+#     digit = str(sum(listofSquares))
+#     return (int(digit[-1]))
+# if __name__ == '__main__':
+#     input_n =int(input())
+#     print(FibanacciNumber(input_n))
--- leetcode/Array/TwoSum.py
+++ leetcode/Array/TwoSum.py
@@ -1,40 +1,22 @@
-"""
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-:: Approach
-
-Assume the first number is 'i' , this makes the second number target - i
-before searching for second number in nums list replace i with a placeholder
-    (number that cant be second  number eg 0.1) in order to maintain the indexing of the numbers
-if number is found append its index and that of i
-
-Else Replace the placeholder( fake number) with its original value with is i
-
-
-
-"""
-
-def twoSum(nums, target):
-    returnList =[]
-    for i in nums:
-        firstindex = nums.index(i)
-        lookingFor = target - i
-        nums.remove(i)
-        if lookingFor in nums:
-            nums.insert(firstindex,0.1)
-            returnList.append(firstindex)
-            returnList.append(nums.index(lookingFor))
-            nums.insert(firstindex, i)
-            return (returnList)
-
-
-        else:
-            nums.insert(firstindex,i)
-
-
-
+class Solution:
+    def twoSum(self,nums, target):
+        returnList =[]
+        for i in nums:
+            firstindex = nums.index(i)
+            lookingFor = target - i
+            nums.remove(i)
+            if lookingFor in nums:
+                nums.insert(firstindex,0.1)
+                returnList.append(firstindex)
+                returnList.append(nums.index(lookingFor))
+                nums.insert(firstindex, i)
+                return (returnList)
+            else:
+                nums.insert(firstindex,i)
 target = 9
 nums = [2,7,11,15]
-twoSum(nums, target)
+
+problem = Solution()
+problem.twoSum(nums, target)
+
+
