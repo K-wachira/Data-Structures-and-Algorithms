b708516c77da544b8184a839d5b2a0525cfa8b14
k-wachira <k.wachira@alustudent.com>
2020-08-02 20:46:51 +0200

    Competition and hackerthones Folder

--- leetcode/LinkedList/Easy/LinkedListCycle.py
+++ leetcode/LinkedList/Easy/LinkedListCycle.py
@@ -10,6 +10,9 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+
+
     # Keep track of the index9 by maybe a counta every time
     # you go to next and when done compare the counta to pos
     # if pos is not between 0 and counta(final value) return False Else TrueWWWWWWW
+
