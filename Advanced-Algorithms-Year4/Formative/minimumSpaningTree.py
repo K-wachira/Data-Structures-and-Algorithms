b6213929db9c954362cf08edc0e0ea3c199efd60
k-wachira <k.wachira@alustudent.com>
2021-12-02 15:08:59 +0200

    dynamic implementation

--- Advanced-Algorithms-Year4/Formative/minimumSpaningTree.py
+++ Advanced-Algorithms-Year4/Formative/minimumSpaningTree.py
@@ -2,44 +2,44 @@ import heapq
 
 class Solution:
     def MST(self, points):
-        if not points or len(points) == 0:
-            return 0
-        size = len(points)
+        n = len(points)
+        if not n or not points:   return 0
         pq = []
-        visited = [False] * size
-        result = 0
-        count = size - 1
-        # Add all edges from points[0] vertexs
-        x1, y1 = points[0]
-        for j in range(1, size):
-            # Calculate the distance between two coordinates.
+        seen = [False] * n
+        total = 0
+        count = n - 1
+
+        # Create a heap of all edges that start form position 1,
+        # in  our case that 2ill be 0
+        p1, p2 = points[0]
+        for j in range(1, n):
             x2, y2 = points[j]
-            cost = abs(x1 - x2) + abs(y1 - y2)
-            edge = Edge(0, j, cost)
+            cost = abs(p1 - x2) + abs(p2 - y2)
+            edge = Edges(0, j, cost)
         
             pq.append(edge)
         
         # Convert pq to a heap.
         heapq.heapify(pq)
 
-        visited[0] = True
+        seen[0] = True
         while pq and count > 0:
             edge = heapq.heappop(pq)
             point1 = edge.point1
             point2 = edge.point2
             cost = edge.cost
-            if not visited[point2]:
-                result += cost
-                visited[point2] = True
-                for j in range(size):
-                    if not visited[j]:
+            if not seen[point2]:
+                total += cost
+                seen[point2] = True
+                for j in range(n):
+                    if not seen[j]:
                         distance = abs(points[point2][0] - points[j][0]) + \
                                    abs(points[point2][1] - points[j][1])
-                        heapq.heappush(pq, Edge(point2, j, distance))
+                        heapq.heappush(pq, Edges(point2, j, distance))
                 count -= 1
-        return result
+        return total
 
-class Edge:
+class Edges:
     def __init__(self, point1, point2, cost):
         self.point1 = point1
         self.point2 = point2
