69473dde99355bd7fedc9f023b381e9ae679a038
k-wachira <k.wachira@alustudent.com>
2021-12-02 15:29:55 +0200

    cleanup

--- Advanced-Algorithms-Year4/Formative/minimumSpaningTree.py
+++ Advanced-Algorithms-Year4/Formative/minimumSpaningTree.py
@@ -22,7 +22,13 @@ class Solution:
         # Convert pq to a heap.
         heapq.heapify(pq)
 
+
+        # start with the first node and mark it as visited/seen
         seen[0] = True
+        
+        # looop the whole tree, untill we have 
+        # n - 1 edges where n is number of vertices
+
         while pq and count > 0:
             edge = heapq.heappop(pq)
             point1 = edge.point1
@@ -31,8 +37,10 @@ class Solution:
             if not seen[point2]:
                 total += cost
                 seen[point2] = True
+        # add all the new neighbours of the new edge to the heap
                 for j in range(n):
                     if not seen[j]:
+                        # append the new neighbours until together with the distance from the current point
                         distance = abs(points[point2][0] - points[j][0]) + \
                                    abs(points[point2][1] - points[j][1])
                         heapq.heappush(pq, Edges(point2, j, distance))
@@ -49,7 +57,7 @@ class Edges:
         return self.cost < other.cost
     
 
-points1 = [[0,0],[2,2],[3,10],[5,2],[7,0]]
+points1 = [[0,0,[2,2,],[3,10],[5,2],[7,0]]
 points2 =  [[0,0],[1,1],[1,0],[-1,1]]
 
 solution1 = Solution()
